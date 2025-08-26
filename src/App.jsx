dd88f57657a013719495370f138bb2bb60311811
Guido Perman <guidoyaelperman@gmail.com>
2025-08-26 17:03:19 -0300

    fix: correct import statement for UIContext to include file extension

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Header } from "./components/Header";
 import { Meals } from "./components/Meals";
 import { CartProvider } from "./store/CartContext";
-import { UIProvider } from "./store/UIContext";
+import { UIProvider } from "./store/UIContext.jsx";
 import { Cart } from './components/Cart'
 import { Checkout } from "./components/Checkout";
 
